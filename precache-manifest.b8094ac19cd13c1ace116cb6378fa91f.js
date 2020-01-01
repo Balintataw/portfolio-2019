ddc8a4ec2ed24ab1a3c5c69f54954c6b033a1156
Balintataw J <balintataw2@gmail.com>
2020-01-01 13:28:48 -0800

    deploy

---
+++ precache-manifest.b8094ac19cd13c1ace116cb6378fa91f.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa1db4c4b77bf5406c47",
+    "url": "/css/app.f2207d71.css"
+  },
+  {
+    "revision": "aa1db4c4b77bf5406c47",
+    "url": "/js/app.e63044f4.js"
+  },
+  {
+    "revision": "32b5410be1872c91c0f3",
+    "url": "/css/chunk-vendors.0d0b6426.css"
+  },
+  {
+    "revision": "32b5410be1872c91c0f3",
+    "url": "/js/chunk-vendors.c3189ce5.js"
+  },
+  {
+    "revision": "c636ab7cfbeb39ed67573ccd2c5b61a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14643c16e23f8c89a138e856ea23f82c",
+    "url": "/index.prod.html"
+  },
+  {
+    "revision": "9abbbe60d65a1bb1d9d78c163912cd20",
+    "url": "/assets/angular_logo.png"
+  },
+  {
+    "revision": "fcb3900c557f8fca9342eb433858ef4e",
+    "url": "/assets/ar_car_screen_01.webp"
+  },
+  {
+    "revision": "558a39f72992172914f702f85a50e426",
+    "url": "/assets/ar_car_screen.webp"
+  },
+  {
+    "revision": "b6485ba022ba7aeb3e1da130d067444e",
+    "url": "/assets/ar_car_screen.jpg"
+  },
+  {
+    "revision": "c7d0d58348f6fa89a31ceef95e4fd5f7",
+    "url": "/assets/cms_admin.webp"
+  },
+  {
+    "revision": "8966978eb1efc1f626390129c55ebe7d",
+    "url": "/assets/ar_car_screen_02.webp"
+  },
+  {
+    "revision": "0d2e714117ab86abb60db50417a7c663",
+    "url": "/assets/cms_blog.webp"
+  },
+  {
+    "revision": "14f7c3f25c089f43a1af2790b6f3523b",
+    "url": "/assets/cms_admin.png"
+  },
+  {
+    "revision": "d4985fc903d6bb63f48939ee88aa48c9",
+    "url": "/assets/code_sample.png"
+  },
+  {
+    "revision": "a365b023666e40feee325f94ec95bac5",
+    "url": "/assets/cms_blog.png"
+  },
+  {
+    "revision": "ff89db2abd71c010f71bf381fdf48911",
+    "url": "/assets/cms_posts.png"
+  },
+  {
+    "revision": "a26b3551b08739b89a1959b2cae2da6e",
+    "url": "/assets/creator-images/phone_blank.png"
+  },
+  {
+    "revision": "9d5ca3001e4ab3142ab840044e406282",
+    "url": "/assets/currency_converter.webp"
+  },
+  {
+    "revision": "2e1d7d60def283f6cbedfdb2e9cadd44",
+    "url": "/assets/laravel_logo.png"
+  },
+  {
+    "revision": "a56de4e9f39acb34fb17536014671289",
+    "url": "/assets/currency_converter.jpg"
+  },
+  {
+    "revision": "ec871e46d0b50f140d5ae73ccdf69663",
+    "url": "/assets/js_logo.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/assets/logo.svg"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/assets/google-play-badge.png"
+  },
+  {
+    "revision": "a50eacad01eb4322742556d21fc4854c",
+    "url": "/assets/meditations_1.webp"
+  },
+  {
+    "revision": "594de3607853e089bb590a7d5619d6c0",
+    "url": "/assets/meditations_2.webp"
+  },
+  {
+    "revision": "527cafee0f8783185e50527638359a55",
+    "url": "/assets/meditations_3.webp"
+  },
+  {
+    "revision": "2939ccc951942a44cac061e8748159d1",
+    "url": "/assets/meditations_3.jpg"
+  },
+  {
+    "revision": "29086a17ac315858ae9e4e32a3729908",
+    "url": "/assets/mysql_logo.png"
+  },
+  {
+    "revision": "36700bee9a1831f2691165d291d62d78",
+    "url": "/assets/nodejs_logo.png"
+  },
+  {
+    "revision": "c4c28519a21a5f0166db9ed480139706",
+    "url": "/assets/panahon_loading.webp"
+  },
+  {
+    "revision": "dedf50e9a58355a350cb8018ee4237b0",
+    "url": "/assets/panahon_home.webp"
+  },
+  {
+    "revision": "f074ba2205f602e898c93a396d866d86",
+    "url": "/assets/panahon_home.jpg"
+  },
+  {
+    "revision": "d4f6bd55b8b42794c995dce6610399a9",
+    "url": "/assets/panahon_loading.jpg"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/assets/react_logo.png"
+  },
+  {
+    "revision": "ff426e349c9a2e5ace06a08b86007630",
+    "url": "/assets/ravenous_home_screen.webp"
+  },
+  {
+    "revision": "9db929ed88e829667df4ad407be9f4b8",
+    "url": "/assets/vegas.jpg"
+  },
+  {
+    "revision": "71e491d8909305534091759bdcb60940",
+    "url": "/assets/street_eats_mobile.png"
+  },
+  {
+    "revision": "86011399807954bf57c34257e73fa622",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "783f891eb23fcf0b7128e0d24a59aaad",
+    "url": "/assets/vue_logo.png"
+  },
+  {
+    "revision": "bc8ce09d646c4a838b6802fa1714b2d6",
+    "url": "/assets/meditations_2.jpg"
+  },
+  {
+    "revision": "a17d4c8c6aa82a556d9b214e2d3c464c",
+    "url": "/assets/meditations_1.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bf2b9a76eeaabc4e224994c267ad41a8",
+    "url": "/assets/sweet_lossy.webp"
+  },
+  {
+    "revision": "e27ceb9b8fa4e4d207d3c27c796aacb4",
+    "url": "/assets/ar_car_screen_02.jpg"
+  },
+  {
+    "revision": "7c49ef1dd767d13f78319dffe6909492",
+    "url": "/assets/ar_car_screen_01.jpg"
+  },
+  {
+    "revision": "5037abc1c4a36e8a5b57f9972e1c7cad",
+    "url": "/assets/sweet.jpg"
+  },
+  {
+    "revision": "6289054c94bba8d5e28214c7efcebe80",
+    "url": "/assets/ravenous_home_screen.png"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,68 @@
+importScripts("/precache-manifest.b8094ac19cd13c1ace116cb6378fa91f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/* eslint-disable no-debugger */
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+workbox.setConfig({
+    debug: false,
+});
+  
+if (workbox) {
+    console.log(`Workbox is loaded`);
+    self.__precacheManifest = [].concat(self.__precacheManifest || []);
+    workbox.precaching.suppressWarnings();
+    workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+} 
+else {
+    console.log(`Workbox failed to load`);
+}
+// workbox.precaching.precacheAndRoute([]);
+
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'images',
+        plugins: [
+        new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+        ],
+    }),
+);
+
+workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'googleapis',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30,
+        }),
+      ],
+    }),
+);
+
+workbox.routing.registerRoute(
+    new RegExp('https://api.github.com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'githubapis',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30,
+        }),
+      ],
+    }),
+);
+
+workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'static-resources',
+    })
+);
+
+self.addEventListener("message", (e)=>{
+    if (e.data.action=='skipWaiting') self.skipWaiting()
+})
+  
